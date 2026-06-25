b0b76264434cb61ff9c3fc107a41d6a43453d487
Quang <thequang012004@gmail.com>
2026-06-25 13:38:20 +0700

    feat: add code for learning widget in learning flutter

---
+++ 2. Flutter/code/widget/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Demo")),
+        body: Center(
+          child: ElevatedButton(onPressed: () {}, child: Text("Nhấn vào đây")),
+        ),
+      ),
+    );
+  }
+}
